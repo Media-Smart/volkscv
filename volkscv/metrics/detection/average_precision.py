35db7e5e3f1531ef4cd1cdacd6ab5182d08a6885
chwang <chenhao.wang@media-smart.cn>
2020-08-03 19:37:27 +0800

    area can be specified

--- volkscv/metrics/detection/average_precision.py
+++ volkscv/metrics/detection/average_precision.py
@@ -5,10 +5,12 @@ from .base import COCOAnalysis
 
 class AveragePrecision(COCOAnalysis):
 
-    def __init__(self, ap_iou=None, maxdets=None):
+    def __init__(self, ap_iou=None, maxdets=None, areaRng=None, areaRngLbl=None):
         super().__init__()
         self.ap_iou = ap_iou
         self.maxdets = maxdets
+        self.areaRng = areaRng
+        self.areaRngLbl = areaRngLbl
 
     def compute(self, pred_path, target_path):
         super().compute(pred_path, target_path)
@@ -18,25 +20,48 @@ class AveragePrecision(COCOAnalysis):
             assert type(self.maxdets) is list, 'maxdets must be a list'
             self.cocoEval.params.maxDets = list(set(self.maxdets))
             self.cocoEval.params.maxDets.sort()
+        if self.areaRng is not None:
+            assert self.areaRngLbl is not None, 'areaRng and areaRngLbl must be match!'
+            assert len(self.areaRng) == len(self.areaRngLbl), 'areaRng and areaRngLbl must be match!'
+            self.cocoEval.params.areaRng = self.areaRng
+            self.cocoEval.params.areaRngLbl = self.areaRngLbl
 
     def accumulate(self):
         super().accumulate()
 
         ap_stats = []
-        if self.ap_iou is None:
+        ap_stats.append(
+            self._summarize(1, maxDets=self.cocoEval.params.maxDets[-1]))
+
+        if (self.ap_iou is None) and (self.areaRng is None):
             # COCO map setting
-            ap_stats.append(
-                self._summarize(1, maxDets=self.cocoEval.params.maxDets[-1]))
             ap_stats.append(
                 self._summarize(1, iouThr=.5, maxDets=self.cocoEval.params.maxDets[-1]))
             ap_stats.append(
                 self._summarize(1, iouThr=.75, maxDets=self.cocoEval.params.maxDets[-1]))
-        else:
             ap_stats.append(
-                self._summarize(1, maxDets=self.cocoEval.params.maxDets[-1]))
+                self._summarize(1, areaRng='small', maxDets=self.cocoEval.params.maxDets[-1]))
+            ap_stats.append(
+                self._summarize(1, areaRng='medium', maxDets=self.cocoEval.params.maxDets[-1]))
+            ap_stats.append(
+                self._summarize(1, areaRng='large', maxDets=self.cocoEval.params.maxDets[-1]))
+
+        else:
+            if self.ap_iou is None:
+                self.ap_iou = [0.5, 0.75]
+            if self.areaRngLbl is None:
+                self.areaRngLbl = ['all', 'small', 'medium', 'large']
+
             for iou in self.cocoEval.params.iouThrs:
                 ap_stats.append(
                     self._summarize(1, iouThr=iou, maxDets=self.cocoEval.params.maxDets[-1]))
+            for label in self.areaRngLbl:
+                ap_stats.append(
+                    self._summarize(1, areaRng=label, maxDets=self.cocoEval.params.maxDets[-1]))
+            for iou in self.cocoEval.params.iouThrs:
+                for label in self.areaRngLbl:
+                    ap_stats.append(
+                        self._summarize(1, iouThr=iou, areaRng=label, maxDets=self.cocoEval.params.maxDets[-1]))
 
         accumulate_state = {
             'map': ap_stats[0],
--- volkscv/metrics/detection/average_recall.py
+++ volkscv/metrics/detection/average_recall.py
@@ -5,10 +5,12 @@ from .base import COCOAnalysis
 
 class AverageRecall(COCOAnalysis):
 
-    def __init__(self, ar_iou=None, maxdets=None):
+    def __init__(self, ar_iou=None, maxdets=None, areaRng=None, areaRngLbl=None):
         super().__init__()
         self.ar_iou = ar_iou
         self.maxdets = maxdets
+        self.areaRng = areaRng
+        self.areaRngLbl = areaRngLbl
 
     def compute(self, pred_path, target_path):
         super().compute(pred_path, target_path)
@@ -18,6 +20,11 @@ class AverageRecall(COCOAnalysis):
             assert type(self.maxdets) is list, 'maxdets must be a list'
             self.cocoEval.params.maxDets = list(set(self.maxdets))
             self.cocoEval.params.maxDets.sort()
+        if self.areaRng is not None:
+            assert self.areaRngLbl is not None, 'areaRng and areaRngLbl must be match!'
+            assert len(self.areaRng) == len(self.areaRngLbl), 'areaRng and areaRngLbl must be match!'
+            self.cocoEval.params.areaRng = self.areaRng
+            self.cocoEval.params.areaRngLbl = self.areaRngLbl
 
     def accumulate(self):
         super().accumulate()
@@ -26,10 +33,29 @@ class AverageRecall(COCOAnalysis):
         for dets in self.cocoEval.params.maxDets:
             ar_stats.append(
                 self._summarize(0, maxDets=dets))
-        for iou in self.cocoEval.params.iouThrs:
-            for dets in self.cocoEval.params.maxDets:
-                ar_stats.append(
-                    self._summarize(0, iouThr=iou, maxDets=dets))
+
+        if (self.ar_iou is None) and (self.areaRng is None):
+            ar_stats.append(
+                self._summarize(0, areaRng='small', maxDets=self.cocoEval.params.maxDets[-1]))
+            ar_stats.append(
+                self._summarize(0, areaRng='medium', maxDets=self.cocoEval.params.maxDets[-1]))
+            ar_stats.append(
+                self._summarize(0, areaRng='large', maxDets=self.cocoEval.params.maxDets[-1]))
+
+        else:
+            if self.ar_iou is None:
+                ar_iou = [0.5, 0.75]
+            else:
+                ar_iou = self.cocoEval.params.iouThrs
+
+            if self.areaRngLbl is None:
+                self.areaRngLbl = ['all', 'small', 'medium', 'large']
+
+            for iou in ar_iou:
+                for label in self.areaRngLbl:
+                    for dets in self.cocoEval.params.maxDets:
+                        ar_stats.append(
+                            self._summarize(0, iouThr=iou, areaRng=label, maxDets=dets))
 
         index = len(self.cocoEval.params.maxDets)
         accumulate_state = {
