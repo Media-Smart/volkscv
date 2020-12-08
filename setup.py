79f5e90379b66632b3ab3392b583c85111890204
hxcai <chxlll@126.com>
2020-12-08 17:40:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ ext_modules = [
 
 setup(
     name='volkscv',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     url='https://github.com/Media-Smart/volkscv',
     author='chwang, jsun, yxzou, hxcai, ycxiong',
